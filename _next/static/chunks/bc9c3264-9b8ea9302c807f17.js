bd446b80831ad4ca92349f7bfeb826c22dad8005
rishatalleva <102984000+rishatalleva@users.noreply.github.com>
2024-09-29 21:23:10 -0600

    First Commit
    First commit

---
+++ _next/static/chunks/bc9c3264-9b8ea9302c807f17.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[582],{5125:function(t,n,a){a.d(n,{uKn:function(){return e}});var u=a(9350);function e(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z"}}]})(t)}}}]);
