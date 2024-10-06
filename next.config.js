4233ca3bbc35b6f41183294726fb94e1a13af703
rishkumaria <33562078+rishkumaria@users.noreply.github.com>
2024-10-05 18:06:08 -0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+ 
+module.exports = {
+  output: 'export',
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**',
+      },
+    ],
+  },
+}
